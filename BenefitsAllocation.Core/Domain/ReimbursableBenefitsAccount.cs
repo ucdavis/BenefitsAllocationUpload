10b5ee6f18d57f9d21d4f09adf9cb1c860dc36e3
Ken Taylor <kentaylor@ucdavis.edu>
2013-10-25 16:38:27 -0700

    Added Views and Controller for Reimbursable Accts

---
+++ BenefitsAllocation.Core/Domain/ReimbursableBenefitsAccount.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCDArch.Core.DomainModel;
+using FluentNHibernate.Mapping;
+
+namespace BenefitsAllocation.Core.Domain
+{
+    public class ReimbursableBenefitsAccount : DomainObjectWithTypedId<string>
+    {
+        // Not a database field.
+        private string _idString;
+        public virtual string IdString
+        {
+            get { return _id.ToString(); }
+            set { _idString = value; }
+        }
+
+        private ReimbursableBenefitsAccountId _id = new ReimbursableBenefitsAccountId();
+        public new virtual ReimbursableBenefitsAccountId Id 
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _orgId;
+        [Display(Name = "Org ID")]
+        [Required]
+        public virtual string OrgId {
+            get
+            {
+                return _orgId;
+            }
+            set
+            {
+                _orgId = value;
+                _id.OrgId = _orgId; 
+            }
+        }
+
+        private string _chart;
+        [Display(Name = "Chart")]
+        [Required]
+        public virtual string Chart
+        {
+            get { return _chart; }
+            set
+            {
+                _chart = value;
+                _id.Chart = _chart;
+            }
+        }
+
+        private string _account;
+        [Display(Name = "Account")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "You must enter an Account Number.")]
+        [StringLength(7, ErrorMessage = "{0} must be seven (7) characters long.", MinimumLength = 7)]
+        public virtual string Account
+        {
+            get
+            {
+                return _account;
+            }
+            set 
+            { 
+                _account = value;
+                _id.Account = _account;
+            }
+        }
+
+        [Display(Name = "Is Active")]
+        [Required]
+        public virtual bool IsActive { get; set; }
+
+        public override bool Equals(Object obj)
+        {
+            return this.Id.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+
+    public class ReimbursableBenefitsAccountId
+    {
+        public virtual string OrgId { get; set; }
+        public virtual string Chart { get; set; }
+        public virtual string Account { get; set; }
+
+        public ReimbursableBenefitsAccountId() {}
+
+        public ReimbursableBenefitsAccountId(string id)
+        {
+            if (!String.IsNullOrEmpty(id))
+            {
+                try
+                {
+                    var pieces = id.Split('|');
+                    OrgId = pieces[0];
+                    Chart = pieces[1];
+                    Account = pieces[2];
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+            var t = obj as ReimbursableBenefitsAccountId;
+            if (t == null)
+                return false;
+            if (OrgId == t.OrgId && Chart == t.Chart && Account == t.Account)
+                return true;
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return (OrgId + "|" + Chart + "|" + Account).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return (OrgId + "|" + Chart + "|" + Account);
+        }
+    }
+
+    public class ReimbursableBenefitsAccountMap : ClassMap<ReimbursableBenefitsAccount>
+    {
+        public ReimbursableBenefitsAccountMap()
+        {
+            Table("ReimbursableBenefitsAccounts");
+            CompositeId(x => x.Id)
+                .KeyProperty(x => x.OrgId, "OrgID")
+                .KeyProperty(x => x.Chart, "Chart")
+                .KeyProperty(x => x.Account, "Account");
+
+            Map(x => x.IdString).Formula("(Select OrgId + '|' + Chart + '|' + Account)").Not.Update().Not.Insert();
+   
+            Map(x => x.OrgId)
+                .Not.Update()
+                .Not.Insert();
+            Map(x => x.Chart)
+                .Not.Update()
+                .Not.Insert();
+            Map(x => x.Account)
+                .Not.Update()
+                .Not.Insert();
+            Map(x => x.IsActive);
+        }
+    }
+}
---
+++ BenefitsAllocationUpload/Controllers/ReimbursableBenefitsAccountController.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using BenefitsAllocation.Core.Domain;
+using BenefitsAllocationUpload.Models;
+using UCDArch.Core.PersistanceSupport;
+
+namespace BenefitsAllocationUpload.Controllers
+{
+    public class ReimbursableBenefitsAccountController : ApplicationController
+    {
+        private readonly IRepositoryWithTypedId<ReimbursableBenefitsAccount, string> _reimbursableBenefitsAccountRepository;
+
+        public ReimbursableBenefitsAccountController(
+            IRepositoryWithTypedId<ReimbursableBenefitsAccount, string> reimbursableBenefitsAccountRepository)
+        {
+            _reimbursableBenefitsAccountRepository = reimbursableBenefitsAccountRepository;
+        }
+        //
+        // GET: /ReimbursableBenefitsAccount/
+
+        /// <summary>
+        /// Return a list of reimburseable benefits accounts for the logged in user's Org. 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            using (var db = new FISDataMartEntities())
+            {
+                var user = Models.User.FindByLoginId(System.Web.HttpContext.Current.User.Identity.Name);
+                var unit = user.Units.FirstOrDefault();
+                var orgId = string.Empty;
+
+                var schoolCodeParameter = new SqlParameter("schoolCode", unit.SchoolCode);
+
+                orgId = db.Database.SqlQuery<string>(
+                    "SELECT dbo.udf_GetOrgIdForSchoolCode(@schoolCode)", schoolCodeParameter).FirstOrDefault();
+
+                var results = _reimbursableBenefitsAccountRepository.Queryable.Where(x => x.OrgId.Equals(orgId)).ToList();
+                ViewData.Model = results;  
+            }
+
+            return View();
+        }
+
+        //
+        // GET: /ReimbursableBenefitsAccount/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /ReimbursableBenefitsAccount/Create
+
+        public ActionResult Create()
+        {
+            using (var db = new FISDataMartEntities())
+            {
+                var user = Models.User.FindByLoginId(System.Web.HttpContext.Current.User.Identity.Name);
+                var unit = user.Units.FirstOrDefault();
+                var orgId = string.Empty;
+
+                var schoolCodeParameter = new SqlParameter("schoolCode", unit.SchoolCode);
+
+                orgId = db.Database.SqlQuery<string>(
+                    "SELECT dbo.udf_GetOrgIdForSchoolCode(@schoolCode)", schoolCodeParameter).FirstOrDefault();
+
+                var model = new ReimbursableBenefitsAccount() {OrgId = orgId, Chart = "3", IsActive = true};
+                ViewData.Model = model;
+            }
+
+            return View();
+        }
+
+        //
+        // POST: /ReimbursableBenefitsAccount/Create
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Create(ReimbursableBenefitsAccount collection)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var newAccount = new ReimbursableBenefitsAccount()
+                        {
+                            OrgId = collection.OrgId,                                
+                            Chart = collection.Chart,
+                            Account = collection.Account,
+                            IsActive = collection.IsActive
+                        };
+
+                    if (_reimbursableBenefitsAccountRepository.Queryable.Any(x => x.IdString.Equals(newAccount.IdString)))
+                    {
+                        Message = "Account \"" + newAccount.IdString + "\" already exists!";
+
+                        return RedirectToAction("Index");
+                    }
+
+                    _reimbursableBenefitsAccountRepository.EnsurePersistent(newAccount);
+
+                    Message = "New account \"" + newAccount.IdString + "\" has been created.";
+
+                    return RedirectToAction("Index");
+                }
+                return View(collection);
+            }
+            catch
+            {
+                 return View(collection);
+            }
+        }
+
+        //
+        // GET: /ReimbursableBenefitsAccount/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ReimbursableBenefitsAccount/Edit/5
+
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        
+        [System.Web.Mvc.HttpPost]
+        public JsonResult Edit(string id, bool isActive)
+        {
+            Result res;
+            if (!string.IsNullOrEmpty(id))
+            {
+                res = new Result {Text = "Received and processed " + id + " successfully."};
+                var myId = new ReimbursableBenefitsAccountId(id);
+
+                var result =
+                    _reimbursableBenefitsAccountRepository.Queryable.FirstOrDefault(
+                        x => x.IdString.Equals(myId.ToString()));
+
+                if (result != null)
+                {
+                    result.IsActive = isActive;
+                    _reimbursableBenefitsAccountRepository.EnsurePersistent(result);
+
+                    res = new Result {Text = "Update succeeded: Received and processed " + id + "."};
+                }
+                else
+                {
+                    res = new Result { Text = "Update failed: Received and unable to update " + id + "!" };
+                }
+            }
+            else
+            {
+                res = new Result { Text = "Update failed: Id was not privided!" };
+            }
+            
+            return Json(res);
+        }
+
+        //
+        // GET: /ReimbursableBenefitsAccount/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ReimbursableBenefitsAccount/Delete/5
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+
+    public class Result
+    {
+        public string Text { get; set; }
+    }
+}
