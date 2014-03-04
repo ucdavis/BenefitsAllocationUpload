ce972eda23abd19969928bb06f0ea657b941df3f
Ken Taylor <kentaylor@ucdavis.edu>
2014-03-04 09:28:12 -0800

    Committed remainder of files

---
+++ BenefitsAllocation.Core/Domain/CentralAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCDArch.Core.DomainModel;
+using FluentNHibernate.Mapping;
+
+namespace BenefitsAllocation.Core.Domain
+{
+    public class CentralAccount : DomainObjectWithTypedId<string>
+    {
+        [Display(Name = "Org Id")]
+        public virtual string OrgId { get; set; }
+
+        [Display(Name = "School Code")]
+        public virtual string SchoolCode { get; set; }
+
+        [Display(Name = "Chart")]
+        public virtual string Chart { get; set; }
+
+        [Display(Name = "Account")]
+        public virtual string Account { get; set; }
+
+        [Display(Name = "Sub Account")]
+        public virtual string SubAccount { get; set; }
+
+        [Display(Name = "Object Consolidation")]
+        public virtual string ObjectConsolidation { get; set; }
+
+        [Display(Name = "Funding Object Consolidation")]
+        public virtual string FundingObjectConsolidation { get; set; }
+
+        [Display(Name = "Sub Object")]
+        public virtual string SubObject { get; set; }
+
+        [Display(Name = "Function Code")]
+        public virtual string FunctionCode { get; set; }
+
+        [Display(Name = "Trans Doc Origin Code")]
+        public virtual string TransDocOriginCode { get; set; }
+
+        [Display(Name = "Op Fund")]
+        public virtual string OpFund { get; set; }
+    }
+
+    public class CentralAccountMap : ClassMap<CentralAccount>
+    {
+        public CentralAccountMap()
+        {
+            Table("CentralAccounts");
+
+            Id(x => x.Id, "PK_CentralAccounts")
+                .GeneratedBy.Assigned();
+            Map(x => x.OrgId);
+            Map(x => x.SchoolCode);
+            Map(x => x.Chart);
+            Map(x => x.Account);
+            Map(x => x.SubAccount);
+            Map(x => x.ObjectConsolidation);
+            Map(x => x.FundingObjectConsolidation);
+            Map(x => x.SubObject);
+            Map(x => x.FunctionCode);
+            Map(x => x.TransDocOriginCode);
+            Map(x => x.OpFund);
+        }
+    }
+}
---
+++ BenefitsAllocationUpload/Controllers/CentralAccountsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BenefitsAllocation.Core.Domain;
+using BenefitsAllocationUpload.Models;
+using UCDArch.Core.PersistanceSupport;
+
+namespace BenefitsAllocationUpload.Controllers
+{
+    public class CentralAccountsController : ApplicationController
+    {
+
+        private readonly IRepository<CentralAccount> _centralAccountRepository;
+
+        public CentralAccountsController(IRepository<CentralAccount> centralAccountRepository)
+        {
+            _centralAccountRepository = centralAccountRepository;
+        }
+
+        //
+        // GET: /CentralAccounts/
+
+        public ActionResult Index()
+        {
+
+            using (var db = new FISDataMartEntities())
+            {
+                var user = Models.User.FindByLoginId(System.Web.HttpContext.Current.User.Identity.Name);
+                var unit = user.Units.FirstOrDefault();
+                var orgId = string.Empty;
+
+                var schoolCodeParameter = new SqlParameter("schoolCode", unit.SchoolCode);
+
+                orgId = db.Database.SqlQuery<string>(
+                    "SELECT dbo.udf_GetOrgIdForSchoolCode(@schoolCode)", schoolCodeParameter).FirstOrDefault();
+                var model = _centralAccountRepository.GetAll().Where(x => x.OrgId.Equals(orgId)).ToList();
+                return View(model);
+            }
+        }
+
+        //
+        // GET: /CentralAccounts/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /CentralAccounts/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /CentralAccounts/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /CentralAccounts/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CentralAccounts/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /CentralAccounts/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CentralAccounts/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
